936690118af0794b0f1ac5173d8facc41432f37a
vlozanog <vjlozano@miuandes.cl>
2025-05-30 19:16:47 -0400

    cancancan

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,10 @@ class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
 
+  rescue_from CanCan::AccessDenied do |exception|
+    redirect_to root_path, alert: "You are not authorized to perform this action."
+  end
+
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
